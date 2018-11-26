ef67bc701b18dc937b7ab242a0a141373f48c16c
TienNT1993 <tien.nguyen@getlinks.co>
2018-11-26 11:55:10 +0700

    init repository

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    facebooAppId: '1765280016915296',
+    facebookAppSecret: '0adc48c22fe7cec11018d14bcbf94800'
+
+}
+module.exports(config)
+
+
---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+express()
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
